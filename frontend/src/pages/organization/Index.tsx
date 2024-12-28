89d2a64724fca676b473628bbe39f7c77dca32ff
my name <laxmi@gmail.com>
2024-12-28 11:09:05 +0530

    style: update organization page layout for improved responsiveness

--- frontend/src/pages/organization/Index.tsx
+++ frontend/src/pages/organization/Index.tsx
@@ -38,7 +38,7 @@ const Index: React.FC = () => {
           </Button>
         </div>
 
-        <div className="bg-white rounded-lg border border-gray-200 overflow-hidden">
+        <div className="bg-white rounded-lg border overflow-x-auto border-gray-200">
           <div className="px-6 py-2 border-b border-gray-200">
             <div className="flex items-center justify-between">
               <div className="">
